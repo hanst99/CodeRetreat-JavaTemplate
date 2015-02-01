6bb97eb7d8cb59c5edcf317b273f1b2da831143c
Hannes Steffenhagen <cubicentertain@gmail.com>
2015-02-01 17:57:41 -0500

    Initial commit

---
+++ src/main/java/coderetreat/GameOfLife.java
@@ -0,0 +1,7 @@
+package coderetreat;
+
+public class GameOfLife {
+  public static void main(String[] args) {
+    //happy hacking
+  }
+}
---
+++ src/test/java/coderetreat/GameOfLifeTest.java
@@ -0,0 +1,11 @@
+package coderetreat;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class GameOfLifeTest {
+  @Test
+  public void fixMe() {
+    assertTrue("Make the tests pass!", false);
+  }
+}
